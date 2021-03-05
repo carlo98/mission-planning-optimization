be34c5895e5c820d40bddc51fd607721c22b7bf1
Carlo Cena <carlo.cena@protonmail.com>
2021-03-05 12:07:27 +0100

    Started working on simplex.py

--- Mission_Optimizer/mission_optimizer.py
+++ Mission_Optimizer/mission_optimizer.py
@@ -1,6 +1,6 @@
 """
 Optimizes plan of an agent by choosing the optimum order of goal to visit in order to maximize the reward obtained while
-minimizing the number of changes of direction required and staying below the maximum time and energy available.
+minimizing the costs required and staying below the maximum time and energy available.
 
 Author: Carlo Cena
 """
@@ -9,35 +9,32 @@ from typing import List
 
 class MissionPlanOptimizer:
 
-    def __init__(self, time_matrix: List[List[int]], energy_matrix: List[List[int]], changes_dir_matrix: List[List[int]],
+    def __init__(self, time_matrix: List[List[int]], energy_matrix: List[List[int]], cost_matrix: List[List[int]],
                  rewards: List[int], difficulties: List[float], max_t: int, max_e: int):
         """
         :param time_matrix: list of lists (n+1)x(n+1) where T[i, j] is the time required to go from goal j to goal i
         :param energy_matrix: list of lists (n+1)x(n+1) where E[i, j] is the energy required to go from goal j to goal i
-        :param changes_dir_matrix: list of lists (n+1)x(n+1) where E[i, j] is the number of changes of direction required to go from goal j to goal i
+        :param cost_matrix: list of lists (n+1)x(n+1) where E[i, j] is the number of changes of direction required to go from goal j to goal i
         :param rewards: list (n+1) where R[i] is the reward obtained by visiting goal i
         :param difficulties: list (n+1) where D[i] is the difficulty associated to goal i
         :param max_t: Integer, maximum time available in seconds
         :param max_e: Integer 0-100, maximum energy available
         With n = number of goals + 1 (starting position).
         """
-        if not isinstance(time_matrix, List) or not isinstance(energy_matrix, List) or not isinstance(changes_dir_matrix, List)\
+        if not isinstance(time_matrix, List) or not isinstance(energy_matrix, List) or not isinstance(cost_matrix, List)\
                 or not isinstance(rewards, List) or not isinstance(difficulties, List) or not isinstance(max_t, int)\
                 or not isinstance(max_e, int):
             print("Check inputs' type.")
             raise TypeError
         self.T = time_matrix
         self.E = energy_matrix
-        self.C = changes_dir_matrix
+        self.C = cost_matrix
         self.max_t = max_t
         self.max_e = max_e
         self.N = len(self.E)
         r = rewards
         d = difficulties
-        self.from_list = []
-        self.to_list = []
         self.__compute_cost_matrix__(r, d)
-        self.__compute_from_to_lists__()
 
     def run(self):
         """
@@ -74,14 +71,3 @@ class MissionPlanOptimizer:
             for j in range(self.N):
                 if i != j:  # We don't want loops
                     self.C[i][j] -= reward[i] * difficulty[i]
-                    
-    def __compute_from_to_lists__(self):
-        """
-        Computes lists needed by predicate 'dreachable' of minizinc.
-        """
-        for i in range(self.N):
-            for j in range(self.N):
-                if j != i:
-                    self.from_list.append(j+1)
-                    self.to_list.append(i+1)
-
--- Mission_Optimizer/simplex.py
+++ Mission_Optimizer/simplex.py
@@ -1,34 +1,82 @@
 """
-Implementation of the simplex algorithm in python3.
+Implementation of the revised simplex algorithm in python3.
 
 Author: Carlo Cena
 """
 from typing import List
+import numpy as np
+import copy
 
 
 class Simplex:
     """
-    Class which implements the simplex algorithm.
+    Class which implements the revised simplex algorithm.
     """
 
-    def __init__(self, a: List[List[float]], c: List[float], x: List[int], b: List[float]):
+    def __init__(self, a: List[List[float]], c: List[float], b: List[float]):
         """
         Given m, number of constraints, and then n, number of variables:
         :param a: mxn matrix of constraints.
         :param c: nx1 vector, cost associated to each variable.
-        :param x: nx1 vectors of variables.
         :param b: nx1 vector for constraints.
         """
-        if not isinstance(a, List) or not isinstance(c, List) or not isinstance(x, List) or not isinstance(b, List):
+        if not isinstance(a, List) or not isinstance(c, List) or not isinstance(b, List):
             print("Check inputs' type.")
             raise TypeError
-        self.A = a
-        self.C = c
-        self.x = x
-        self.b = b
+        self.A = np.array(a)
+        self.c = np.array(c)
+        self.b = np.array(b)
 
     def run(self) -> List[float]:
         """
         :return: Optimal solution of given problem.
         """
-        pass
+        x = np.zeros(len(self.c))
+        B, N, cb, cn, indexes = self.__find_basis__()
+        xb = np.dot(np.linalg.inv(B), self.b)
+        l = np.dot(np.linalg.inv(np.transpose(B)), cb)
+        sn = cn - np.dot(np.transpose(N), l)
+        q = 0
+        while np.sum(sn) < 0:
+            if not self.c[q] < 0:
+                q += 1
+                continue
+
+            d = np.dot(np.linalg.inv(B), N[:, q])
+            if np.sum(d) <= 0:
+                print("Unbounded problem.")
+                return []
+            db = np.dot(np.linalg.inv(B), N[:, q])*x[q]
+            while not np.min(xb - db) <= 0:
+                x[q] += 1
+                db = np.dot(np.linalg.inv(B), N[:, q])*x[q]
+
+            p = int(np.argmin(xb - db))
+            tmp = copy.deepcopy(B[:, :])
+            tmp[:, p] = N[:, q]
+            N[:, q] = B[:, p]
+            B = tmp[:, :]
+            indexes[p] = q
+            cb = self.c[[x for x in indexes.values()]]
+            cn = self.c[[y not in [x for x in indexes.values()] for y in range(len(self.c))]]
+
+            xb = np.dot(np.linalg.inv(B), self.b)
+            l = np.dot(np.linalg.inv(np.transpose(B)), cb)
+            sn = cn - np.dot(np.transpose(N), l)
+
+            q += 1
+
+        return list(x)
+
+    def __find_basis__(self):
+        """
+        Finds a basis B for matrix A and returns matrix N and vectors cb, cn and dictionary of indexes taken.
+        """
+        B = self.A[:, -len(self.A):]
+        N = self.A[:, :-len(self.A)]
+        cb = self.c[-len(self.A):]
+        cn = self.c[:-len(self.A)]
+        indexes = dict()
+        for i in range(len(self.A)):
+            indexes[i] = len(self.A[0]) - len(self.A) + i
+        return B, N, cb, cn, indexes
