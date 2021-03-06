7c7a887fa001845e36f99622483c6c173928ed74
Carlo Cena <carlo.cena@protonmail.com>
2021-03-06 17:00:22 +0100

    Connectivity constraints missing

--- Mission_Optimizer/mission_optimizer.py
+++ Mission_Optimizer/mission_optimizer.py
@@ -5,6 +5,8 @@ minimizing the costs required and staying below the maximum time and energy avai
 Author: Carlo Cena
 """
 from typing import List
+import numpy as np
+from Mission_Optimizer.simplex import Simplex
 
 
 class MissionPlanOptimizer:
@@ -21,29 +23,33 @@ class MissionPlanOptimizer:
         :param max_e: Integer 0-100, maximum energy available
         With n = number of goals + 1 (starting position).
         """
-        if not isinstance(time_matrix, List) or not isinstance(energy_matrix, List) or not isinstance(cost_matrix, List)\
-                or not isinstance(rewards, List) or not isinstance(difficulties, List) or not isinstance(max_t, int)\
+        if not isinstance(time_matrix, List) or not isinstance(energy_matrix, List) or not isinstance(cost_matrix, List) \
+                or not isinstance(rewards, List) or not isinstance(difficulties, List) or not isinstance(max_t, int) \
                 or not isinstance(max_e, int):
             print("Check inputs' type.")
             raise TypeError
-        self.T = time_matrix
-        self.E = energy_matrix
-        self.C = cost_matrix
+        self.T = np.array(time_matrix)
+        self.E = np.array(energy_matrix)
         self.max_t = max_t
         self.max_e = max_e
         self.N = len(self.E)
-        r = rewards
-        d = difficulties
-        self.__compute_cost_matrix__(r, d)
+        self.R = np.array(rewards)
+        self.D = np.array(difficulties)
+        self.Costs = np.array(cost_matrix)
+
+        # Creating data structures for simplex algorithm
+        self.__simplex_structures__()
 
     def run(self):
         """
         Uses branch and bound and simplex class to get optimal solution of
         constrained optimization problem.
-        :return result.solution is the best solution, if no solution exists result.solution is None.
+        :return result is a dictionary with the best solution and its value, if no solution exists result is None.
         """
-        pass
-        
+        solver = Simplex(list(self.A), list(self.c), list(self.b))
+        solution = solver.run()
+        print(solution)
+
     def extract_path(self, result):
         """
         :param result: Result obtained from MissionPlanOptimizer.run, it should not be None.
@@ -62,12 +68,94 @@ class MissionPlanOptimizer:
                     break
         return optimum_path
 
-    def __compute_cost_matrix__(self, reward, difficulty):
+    def __simplex_structures__(self):
         """
-        :param reward: reward array
-        :param difficulty: difficulty array
+        Creates matrix A and vectors b and c for simplex algorithm.
         """
+        self.__compute_cost_vector__()
+        self.__create_A_b__()
+
+    def __compute_cost_vector__(self):
+        """
+        Computes the cost array for simplex.
+        """
+        self.c = np.array(self.Costs, dtype=np.float64)
         for i in range(self.N):
             for j in range(self.N):
                 if i != j:  # We don't want loops
-                    self.C[i][j] -= reward[i] * difficulty[i]
+                    self.c[i][j] = self.Costs[i][j] - self.R[i] * (1 - self.D[i])
+        print(self.c)
+        self.c = self.c.flatten()
+
+    def __create_A_b__(self):
+        """
+        Creates matrix A and vector b for constraints, to be used by simplex algorithm.
+        Constraints, given X solution vector and xi its components:
+        1) TX <= max_t
+        2) EX <= max_e
+        3) Goal 0 is starting point
+        4) Avoid loop in goal 0
+        5) 0 <= xi <= 1
+        6) Every goal can be reached only once
+        7) Every goal can have only a destination
+        8) connectivity constraints
+        """
+        self.A = []
+        self.b = []
+
+        self.A.append(list(self.T.flatten()))  # (1)
+        self.b.append(self.max_t)
+
+        self.A.append(list(self.E.flatten()))  # (2)
+        self.b.append(self.max_e)
+
+        self.A.append(list(np.zeros(len(self.c))))  # (3.1)
+        self.b.append(1.)
+        for i in range(len(self.R)):
+            self.A[-1][i * len(self.R)] = 1.
+
+        self.A.append(list(np.zeros(len(self.c))))  # (3.2)
+        self.b.append(1.)
+        for i in range(len(self.R)):
+            self.A[-1][i * len(self.R)] = 1.
+
+        self.A.append(list(np.zeros(len(self.c))))  # (4)
+        self.b.append(0.)
+        for i in range(len(self.R)):
+            self.A[-1][i] = 1.
+
+        for i in range(len(self.c)):  # (5)
+            self.A.append(list(np.zeros(len(self.c))))
+            self.A[-1][i] = 1.
+            self.b.append(1.)
+
+        for i in range(1, len(self.R)):  # (6)
+            self.A.append(list(np.zeros(len(self.c))))
+            self.b.append(1.)
+            for j in range(len(self.R)):
+                self.A[-1][i * len(self.R) + j] = 1.
+
+        for i in range(len(self.R)):  # (7)
+            self.A.append(list(np.zeros(len(self.c))))
+            self.b.append(1.)
+            for j in range(len(self.R)):
+                self.A[-1][i + j * len(self.R)] = 1.
+
+        last_no_connectivity = len(self.A)
+
+        # TODO: Connectivity constraints
+
+        for i in range(len(self.A)):  # Adding slack variables
+            for j in range(len(self.A)):
+                if i == j:
+                    if i > last_no_connectivity or i == 2:
+                        self.A[i].append(-1.)
+                    else:
+                        self.A[i].append(1.)
+                else:
+                    self.A[i].append(0.)
+
+        self.c = list(self.c) + list(np.zeros(len(self.A[0]) - len(self.c)))
+
+        self.b = np.array(self.b)
+        self.A = np.array(self.A)
--- example.py
+++ example.py
@@ -7,51 +7,20 @@ in order to maximise the prize and minimize the number of changes of direction.
 """
 from Mission_Optimizer.mission_optimizer import MissionPlanOptimizer
 
-Time = [[0, 5, 10, 10, 7],
-        [5, 0, 10, 10, 10],
-        [10, 10, 0, 5, 5],
-        [10, 10, 5, 0, 5],
-        [7, 10, 5, 5, 0]]
-Energy = [[0, 1, 1, 1, 1],
-          [1, 0, 1, 1, 1],
-          [1, 1, 0, 1, 1],
-          [1, 1, 1, 0, 1],
-          [1, 1, 1, 1, 0]]
-Changes = [[0, 5, 10, 10, 7],
-           [5, 0, 10, 10, 10],
-           [10, 1, 0, 2, 5],  # Play with 2 element of this row to see changes in result and test cycles
-           [10, 10, 5, 0, 2],
-           [7, 10, 2, 5, 0]]
-Rewards = [0, 6, 6, 6, 6]
-Difficulties = [0, 0.5, 0.5, 0.5, 0.5]
+Time = [[0, 1, 1],
+        [1, 0, 1],
+        [1, 1, 0]]
+Energy = [[0, 1, 1],
+          [1, 0, 1],
+          [1, 1, 0]]
+Changes = [[0, 1, 1],
+           [1, 0, 6],
+           [5, 4, 0]]
+Rewards = [0, 6, 5]
+Difficulties = [0, 0, 0]
 max_t = 20
 max_e = 90
 
 optim = MissionPlanOptimizer(Time, Energy, Changes, Rewards, Difficulties, max_t, max_e)
-print("C matrix")
-for i in range(len(optim.C)):
-    print(optim.C[i])
 
 result = optim.run()
-
-# Accessing variables
-if result is not None:
-    print("Edges")
-    for i in range(len(result["G"])):
-        print(result["G"][i])
-    print("Total time")
-    print(result["tot_time"])
-    print("Total energy")
-    print(result["tot_energy"])
-    print("Minimized value")
-    print(result["value"])
-    print("Optimum path")
-    optim_path = optim.extract_path(result)
-    print(optim_path)
-    r = 0
-    for i in optim_path:
-        r += Rewards[i]
-    print("Collected Reward")
-    print(r)
-else:
-    print("No solution has been found.")
